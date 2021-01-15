794d321817ebaf66b529a18abf59a983bc17f408
Sérgio Saraiva <sergio.s.neto@outlook.com>
2021-01-15 09:28:26 -0300

    route-001 is working

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,7 @@ const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ msg: 'Hello World' }));
 
+routes.get('/rota-001', (req, res) => {res.json({ result: 1+1})});
+
 
 export default routes;
