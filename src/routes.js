ad33353d98854661f0e281d57e61f32a6ac97281
Sérgio Saraiva <sergio.s.neto@outlook.com>
2021-01-15 09:30:02 -0300

    now route-001 add numbers

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ msg: 'Hello World' }));
 
-routes.get('/rota-001', (req, res) => {res.json({ result: 1+1})});
+routes.get('/rota-001/:num', (req, res) => {
+    const {num} = req.params    
+    res.json({ result: Number(num)+1})
+});
 
 
 export default routes;
