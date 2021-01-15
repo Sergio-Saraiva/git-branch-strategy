49b671795bdeab49a83c13dff01c5cbf69b86c5c
Sérgio Saraiva <sergio.s.neto@outlook.com>
2021-01-15 09:39:09 -0300

    route-002 returns hellor world and a number

--- src/routes.js
+++ src/routes.js
@@ -8,8 +8,9 @@ routes.get('/rota-001/:num', (req, res) => {
     res.json({ result: Number(num)+1})
 });
 
-routes.get('/rota-002', (req, res) => {
-    return res.send('<h1>Hellor World</h1>')
+routes.get('/rota-002/:num', (req, res) => {
+    const {num} = req.params   
+    return res.send(`<h1>Hellor World: ${num}</h1>`)
 })
 
 routes.get('/rota-003/:num', (req, res) => {
